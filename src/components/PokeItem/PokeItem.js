6de071463523596469b02b387f2011f3e3f35ac4
Daniel Garcia <daosgava.garcia@gmail.com>
2019-06-12 19:24:46 -0500

    removing some coins icon

--- src/components/PokeItem/PokeItem.js
+++ src/components/PokeItem/PokeItem.js
@@ -8,7 +8,7 @@ class PokeItem extends React.Component{
                     <p className="title">{name}  <img src={image} alt={name}/></p>
                     <small>{description.short_effect}</small>
                     <br></br>
-                    <p className='price'>{cost} <i className="nes-icon coin is-small"></i><i className="nes-icon coin is-small"></i><i className="nes-icon coin is-small"></i></p>
+                    <p className='price'>{cost} <i className="nes-icon coin is-small"></i></p>
                     {
                         addToCart ? <button className='main-button nes-btn is-success bottom-down' onClick={()=>addToCart({id:id, name:name, description:description, image:image, cost:cost})}>Add</button> : <button className='main-button is-warning nes-btn bottom-down' onClick={()=>removeFromCart({id:id})}>Remove</button>
                     }
